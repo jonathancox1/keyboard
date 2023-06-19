7445a73bd86e796d01e02cbed3a6cf716aada2ad
Jonathan Cox <cox.jonathan@gmail.com>
2023-06-19 15:13:56 -0400

    adds homerow mods

--- keymap.c
+++ keymap.c
@@ -55,7 +55,6 @@ tap_dance_action_t tap_dance_actions[] = {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
     /* Qwerty
      * ,-----------------------------------------------------------------------------------.
      * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   `  |
@@ -70,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_GRV, 
-    TD(TD_ESC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT, 
+    TD(TD_ESC_CAPS), KC_A, CTL_T(KC_S), ALT_T(KC_D), CMD_T(KC_F), KC_G, KC_H, CTL_T(KC_J), ALT_T(KC_K), CMD_T(KC_L), KC_SCLN, KC_ENT, 
     KC_LSFT, KC_Z, KC_X, KC_C, LT(_NUMPAD, KC_V), KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,
     _______, KC_LCTL, KC_LALT, TD(TD_CMD_ENT), LOWER, KC_BSPC, KC_SPC, RAISE, LT(_SYMBOLS, KC_LEFT), KC_DOWN, KC_UP, KC_RIGHT
     ),
@@ -108,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     
     [_RAISE] = LAYOUT_planck_grid(
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_HOME, KC_UP, KC_END, KC_NO, KC_NO, 
-        KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_APP, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_NO,
+        KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_NO,
         _______, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_WH_U, KC_WH_D, KC_NO, KC_NO, KC_NO,
         _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
         ),
@@ -255,14 +254,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case PASS:
             if (record->event.pressed) {
-                SEND_STRING("XXX");
+                SEND_STRING("Brandnewpassword1!");
             }
             return false;
             break;
 
         case PASS1:
             if (record->event.pressed) {
-                SEND_STRING("XXX");
+                SEND_STRING("Brandnewpassword1!1!");
             }
             return false;
             break;
